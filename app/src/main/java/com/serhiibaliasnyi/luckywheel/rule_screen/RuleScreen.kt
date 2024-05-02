fa57cb5115803cb67acc2e05d7372f980a683629
Baliasnyi Serghii <sergey070373@gmail.com>
2024-05-02 23:22:10 +0100

    ok1

--- app/src/main/java/com/serhiibaliasnyi/luckywheel/rule_screen/RuleScreen.kt
+++ app/src/main/java/com/serhiibaliasnyi/luckywheel/rule_screen/RuleScreen.kt
@@ -1,5 +1,8 @@
 package com.serhiibaliasnyi.luckywheel.rule_screen
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,7 +17,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
@@ -23,18 +31,37 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.serhiibaliasnyi.luckywheel.R
 import com.serhiibaliasnyi.luckywheel.ui.theme.Red
+import kotlin.math.roundToInt
 
 @Composable
 fun RuleScreen() {
+    var rotationValue by remember {
+        mutableStateOf(0f)
+    }
+
+    var number by remember{
+          mutableStateOf(0)
+    }
+    val angle: Float by animateFloatAsState(
+        targetValue = rotationValue,
+        animationSpec = tween(
+            durationMillis = 2000,
+            easing = LinearOutSlowInEasing
+        ),
+        finishedListener = {
+             number=((360f-(it%360))/(360f/8)).roundToInt()
+        }
+    )
     Column(
         modifier=Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
     ){
-        Text(modifier=Modifier.fillMaxWidth()
+        Text(modifier= Modifier
+            .fillMaxWidth()
             .height(100.dp)
             .wrapContentWidth()
             .wrapContentHeight(),
-            text="0",
+            text=number.toString(),
             fontWeight = FontWeight.Bold,
             fontSize = 35.sp,
             color= Color.White
@@ -45,13 +72,20 @@ fun RuleScreen() {
             Image(
                 painter= painterResource(id = R.drawable.lucky_wheel_bg),
                 contentDescription = "lucky wheel",
-                modifier = Modifier.fillMaxSize().padding(10.dp)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp)
+                    .rotate(angle)
             )
         }
 
-        Button(onClick = {},
+        Button(onClick = {
+                         rotationValue=(720..1080).random().toFloat()+angle
+        },
             colors=ButtonDefaults.buttonColors(Red),
-            modifier = Modifier.fillMaxWidth().padding(10.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
             ){
             Text(text="Start",
                 color= White)
